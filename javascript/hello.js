96068b40f17e4c193166f1c463f584a44f28e711
Abdullah <almahmuda5@gmail.com>
2023-07-28 19:53:17 +0600

    first

---
+++ javascript/hello.js
@@ -0,0 +1,11 @@
+console.log('Hello,World');
+
+var frinedsName = "Rakibul Hasan";
+var number = 10;
+var float = 20.3;
+var char = 'A'
+
+console.log(typeof frinedsName);
+console.log(typeof number);
+console.log(typeof float);
+console.log(typeof char);
